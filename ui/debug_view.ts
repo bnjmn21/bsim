d23b89194c982187336d22461372c9ad617b01fb
bnjmn21 <68250874+bnjmn21@users.noreply.github.com>
2024-08-04 17:31:45 +0200

    dbg input view update

--- ui/debug_view.ts
+++ ui/debug_view.ts
@@ -149,7 +149,7 @@ export function debugViewPlugin(world: World) {
             ctx.beginPath();
             ctx.arc(ptr.x, ptr.y, 40 + ((ptr.width + ptr.height) / 4), 0, Math.PI * 2);
             ctx.fill();
-            ctx.fillText(`buttons: ${ptr.buttons}, type: ${ptr.pointerType}`, ptr.x + 100, ptr.y + 100);
+            ctx.fillText(`${ptr.button}, buttons: ${ptr.buttons}, type: ${ptr.pointerType}`, ptr.x + 100, ptr.y + 100);
         }
     
         ctx.font = `20px "JetBrains Mono", monospace`;
